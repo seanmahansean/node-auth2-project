edc423c5e68dd04e32f1fe122aa49e1bfeed67b2
sean-mahan <seanmahansean@gmail.com>
2021-10-12 01:46:30 -0500

    token stuff

--- api/auth/auth-middleware.js
+++ api/auth/auth-middleware.js
@@ -1,4 +1,7 @@
 const { JWT_SECRET } = require("../secrets"); // use this secret!
+const User = require("../users/users-model")
+const jwt = require("jsonwebtoken");
+const { off } = require("superagent");
 
 const restricted = (req, res, next) => {
   /*
@@ -16,7 +19,19 @@ const restricted = (req, res, next) => {
 
     Put the decoded token in the req object, to make life easier for middlewares downstream!
   */
-  next()
+  const token = req.headers.authorization
+  if(!token){
+    next({status: 401, message: "Token required"})
+  }else{
+    jwt.verify(token, JWT_SECRET, (err, decodedToken) => {
+      if(err){
+        next({status: 401, message: "Token invalid"})
+      }else{
+        req.decodedToken = decodedToken
+        next()
+      }
+    })
+  }
 }
 
 const only = role_name => (req, res, next) => {
@@ -34,7 +49,7 @@ const only = role_name => (req, res, next) => {
 }
 
 
-const checkUsernameExists = (req, res, next) => {
+const checkUsernameExists = async (req, res, next) => {
   /*
     If the username in req.body does NOT exist in the database
     status 401
@@ -42,7 +57,17 @@ const checkUsernameExists = (req, res, next) => {
       "message": "Invalid credentials"
     }
   */
-  next()
+  try{
+    const [user] = await User.findBy({username: req.body.username})
+    if(!user){
+      next({status: 422, message: "Invalid credentials"})
+    }else{
+      req.user = user
+      next()
+    }
+  }catch(err){
+    next(err)
+  }
 }
 
 
--- api/auth/auth-router.js
+++ api/auth/auth-router.js
@@ -2,7 +2,8 @@ const router = require("express").Router();
 const { checkUsernameExists, validateRoleName } = require('./auth-middleware');
 const { JWT_SECRET } = require("../secrets"); // use this secret!
 const bcrypt = require("bcryptjs")
-const User = require("../users/users-model")
+const User = require("../users/users-model");
+const jwt = require("jsonwebtoken")
 
 router.post("/register", validateRoleName, (req, res, next) => {
   /**
@@ -49,6 +50,27 @@ router.post("/login", checkUsernameExists, (req, res, next) => {
       "role_name": "admin" // the role of the authenticated user
     }
    */
+  if(bcrypt.compareSync(req.body.password, req.user.password)){
+    const token = buildToken(req.user)
+    res.json({
+      message: `${req.user.username} is back!`,
+      token
+    })
+  }else{
+    next({status: 401, message: "Invalid credentials"})
+  }
 });
+
+function buildToken(user){
+  const payload = {
+    subject: user.user_id,
+    role_name: user.role_name,
+    username: user.username
+  }
+  const options = {
+    expiresIn: "id"
+  }
+  return jwt.sign(payload, JWT_SECRET, options)
+}
 
 module.exports = router;
